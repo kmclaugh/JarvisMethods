d881a9c1ad0c96b8083d61f490641daca3d2f2f2
Kevin McLaughlin <kevin.mclaughlin70@gmail.com>
2012-10-17 13:01:19 -0400

    Folder with Jarvis Methods
    Currently only contains textsender.py

--- textsender.py
+++ textsender.py
@@ -1,3 +1,5 @@
+#!/Library/Frameworks/Python.framework/Versions/3.2/bin/python3.2
+
 def textsender(number,message):
 
     import smtplib
@@ -7,4 +9,7 @@ def textsender(number,message):
     server.login( 'antonshipley@gmail.com', 'F=kqq/r2' )
 
     server.sendmail( 'kevin', '{0}@mms.att.net'.format(number), '{0}'.format(message) )
+
+
+
 
